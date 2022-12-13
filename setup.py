be36380e839422ada63e194dc77a9d764f8d9ba9
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2022-12-13 09:18:37 -0300

    update project

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-"""
-    Setup file for sheets_factory.
-    Use setup.cfg to configure your project.
-
-    This file was generated with PyScaffold 4.3.1.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
-"""
 from setuptools import setup
 
 setup(
@@ -18,14 +10,14 @@ setup(
     install_requires=open("requirements.txt", "r").read().split(),
 )
 
-if __name__ == "__main__":
-    try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
-    except:  # noqa
-        print(
-            "\n\nAn error occurred while building the project, "
-            "please ensure you have the most updated version of setuptools, "
-            "setuptools_scm and wheel with:\n"
-            "   pip install -U setuptools setuptools_scm wheel\n\n"
-        )
-        raise
+# if __name__ == "__main__":
+#     try:
+#         setup(use_scm_version={"version_scheme": "no-guess-dev"})
+#     except:  # noqa
+#         print(
+#             "\n\nAn error occurred while building the project, "
+#             "please ensure you have the most updated version of setuptools, "
+#             "setuptools_scm and wheel with:\n"
+#             "   pip install -U setuptools setuptools_scm wheel\n\n"
+#         )
+#         raise
